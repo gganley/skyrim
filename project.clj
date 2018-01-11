c77af67a4173e5a950496ad3c0298c9bd99fb9cb
gganley <gganley@student.bridgew.edu>
2018-01-11 12:46:36 -0500

    Init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject skyrim "0.0.1"
+  :description "Find the most ideal composition of ingredients to discover all the effects"
+  :url "gganley.github.io"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/math.combinatorics "0.1.4"]])
