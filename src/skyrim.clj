97aee8582f3640e2ae65bb51f61cb47d9ebb4421
gganley <gganley@student.bridgew.edu>
2018-01-11 13:54:07 -0500

    ASDF

--- src/skyrim.clj
+++ src/skyrim.clj
@@ -4,5 +4,35 @@
             [clojure.algo.generic.functor :refer [fmap]]
             [clojure.math.combinatorics :refer [combinations partitions]]))
 
+(def ingredient-effects-map
+  (with-open [reader (io/reader "alchemy-ingredients.csv")]
+    (into {}
+          (map
+           (fn [x] {(first x) (take 4 (rest x))})
+           (doall
+            (csv/read-csv reader :separator \;))))))
+
+(def remaining (atom ingredient-effects-map))
+
+(defn make-potion
+  "takes as argument a list of ingredients and returns a set of the effects"
+  [ingredient-seq & {:keys [use-remaining removed-effects remove-remaining]
+                     :or {use-remaining nil removed-effects nil remove-remaining nil}}]
+  (let [remaining-effects (map set (vals (select-keys @remaining use-remaining))) ;; [set]
+        full-effects (map set (vals (select-keys ingredient-effects-map ingredient-seq)))
+        effs (concat remaining-effects full-effects)
+        potion (map #(apply clojure.set/union (map clojure.set/intersection %)) (map set (combinations effs 2)))
+        rem-d (map #(clojure.set/intersection % (set (apply concat potion))) remaining-effects)]
+    (if remove-remaining
+      (doseq [ing use-remaining
+             eff potion]
+        (swap! remaining (fn [x] (update x ing #(remove #{eff} %)))))
+      nil)
+    (if removed-effects
+      rem-d
+      potion)))
 
 
+(defn f [x]
+  (let [[q p] x]
+    (make-potion p :use-remaining q :removed-effects true)))
