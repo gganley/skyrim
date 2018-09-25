4cdf33a5df771f59c2ba39a939d9c5eea9a15db2
gganley <gganley@student.bridgew.edu>
2018-09-25 14:59:52 -0400

    Remove unneeded space

--- main.go
+++ main.go
@@ -3,86 +3,23 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"math"
-	"os"
-	"sort"
 	"strings"
 )
 
-func RemoveDuplicates(xs *[]string) {
-	found := make(map[string]bool)
-	j := 0
-	for i, x := range *xs {
-		if !found[x] {
-			found[x] = true
-			(*xs)[j] = (*xs)[i]
-			j++
-		}
-	}
-	*xs = (*xs)[:j]
-}
-
 func main() {
-	file, err := ioutil.ReadFile("ingredients.csv")
+	file, err := ioutil.ReadFile("ing.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	r := csv.NewReader(strings.NewReader(string(file)))
-	r.Comma = ';'
 
-	fullMap := make(map[string][]string)
-	funkyMask := make(map[string]uint64)
-
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fullMap[record[0]] = record[1:5]
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	var effects []string
-	var ingredients []string
-
-	for k, v := range fullMap {
-		effects = append(effects, v...)
-		ingredients = append(ingredients, k)
-	}
-
-	RemoveDuplicates(&effects)
-
-	sort.Strings(effects)
-
-	for i, v := range effects {
-		funkyMask[v] = uint64(math.Pow(2, float64(i)))
-	}
-
-	finalMaskingThing := make(map[string]uint64)
-
-	for k, v := range fullMap {
-		var sum uint64 = 0
-
-		for _, entry := range v {
-			sum += funkyMask[entry]
-		}
-
-		finalMaskingThing[k] = sum
-	}
-	fmt.Println(fullMap)
-	w := csv.NewWriter(os.Stdout)
-	for k, v := range fullMap {
-		if err := w.Write(append([]string{k}, v...)); err != nil {
-			log.Fatalln("error: ", err)
-		}
-	}
-
-	w.Flush()
+	fmt.Println(records)
 }
