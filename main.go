0f80b0601609b709ea03e5a0aec1a4f69500b622
gganley <gganley@student.bridgew.edu>
2018-10-09 22:53:10 -0400

    Add docs

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"math/bits"
+	"os"
 	"sync"
 )
 
@@ -14,13 +15,16 @@ import (
 type Ingredient struct{ disc, all uint64 } // Disc holds all of the
 type Effect uint64                         // 2^i where i is the index of the effect
 
+// This is my ever nice term for the return type of f()
 type FunkyType struct {
 	NumberOfDiscoveredEffects int
 	Potion                    uint64
 	IngredientNames           []string
 }
 
+// The function that determines all the crucial information
 func f(ings []string, Im map[string]*Ingredient) FunkyType {
+	// Determine the complete possible potion
 	gamma := Im[ings[0]].all&Im[ings[1]].all |
 		Im[ings[0]].all&Im[ings[2]].all |
 		Im[ings[1]].all&Im[ings[2]].all
@@ -29,20 +33,31 @@ func f(ings []string, Im map[string]*Ingredient) FunkyType {
 	b := Im[ings[1]].disc & gamma
 	c := Im[ings[2]].disc & gamma
 
+	// All the ingredients that will be discovered
 	q := bits.OnesCount64(a) + bits.OnesCount64(b) + bits.OnesCount64(c)
 	return FunkyType{q, gamma, ings}
 }
 
 func main() {
-
+	// `Im` is the Inredient Map, basically because reading binary data gets old after a while
 	Im, _ := discoverImEm()
-	WriteNonDuds(Im)
+
+	// Get/Make the non-dud potions
+	_, err := os.Open("nonduds.csv")
+	if os.IsNotExist(err) {
+		WriteNonDuds(Im)
+	}
 	nonDuds := getNonDuds()
 
+	// The list of potions that determine all the ingredients
 	var retVal [][]string
 
 	for effectsLeft(Im) {
+		// The channel that all the results of each iteration of f() is sent to
 		result := make(chan FunkyType, 8500)
+
+		// This is a sync utility that lets me manage the lifetime of the channel by incrementing it when a new process
+		// is added and decrementing it when its done calculating it
 		var wg sync.WaitGroup
 		for _, v := range nonDuds {
 			wg.Add(1)
@@ -53,6 +68,7 @@ func main() {
 			}(val, Im)
 		}
 
+		// This runs when all the goroutines that run f() have finished, then contines to the next for-loop
 		go func() {
 			wg.Wait()
 			close(result)
@@ -68,30 +84,17 @@ func main() {
 				potionToBeSubtracted = v.Potion
 			}
 		}
+
 		for _, v := range maxCombo {
+			// Turn off the discovered bits in each ingredient
 			Im[v].disc = Im[v].disc &^ potionToBeSubtracted
 		}
 
+		// Append the found best potion to the return value
 		retVal = append(retVal, maxCombo)
 	}
-	// I really need to figure out if I want to index by the string name of the ingredient or weirdly by the value it
-	// holds
 
-	// ingsmap := make(map[string]int)
-	//
-	// for _, v := range retVal {
-	// 	for _, val := range v {
-	// 		ingsmap[val]++
-	// 	}
-	// 	fmt.Println(v)
-	// }
-	// total := 0
-	// for k, v := range ingsmap {
-	// 	total += v
-	// 	fmt.Println(k, v)
-	// }
-	// fmt.Println(total)
-
+	// Printing the result
 	for _, v := range retVal {
 		fmt.Println(v)
 	}
