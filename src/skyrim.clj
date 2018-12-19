5694e2b9654ef281a2c7b21a2903ae2f1fe3d5dd
gganley <gganley@student.bridgew.edu>
2018-12-19 08:39:15 -0500

    Move to deps.edn and use core.async

--- src/skyrim.clj
+++ src/skyrim.clj
@@ -1,11 +1,6 @@
 (ns skyrim
-  (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+  (:require [clojure.core.async :as async]))
 
-(def ie-map
-  (with-open [reader (io/reader "alchemy-ingredients.csv")]
-    (into {}
-          (map
-           (fn [x] {(first x) (set (take 4 (rest x)))})
-           (doall
-            (csv/read-csv reader :separator \;))))))
+
+
+
