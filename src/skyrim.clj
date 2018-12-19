95176beabc97a6ec8c5499329f3852a1a0e3c4fa
gganley <gganley@student.bridgew.edu>
2018-12-19 12:08:20 -0500

    Read CSV file and format it

--- src/skyrim.clj
+++ src/skyrim.clj
@@ -4,4 +4,9 @@
             [clojure.data.csv :as csv]))
 
 
+(defn sanatize-keyword [^String input]
+  (keyword (clojure.string/replace (clojure.string/lower-case input) #" " "-")))
 
+(def ie-raw
+  (with-open [reader (io/reader "alchemy-ingredients.csv")]
+    (map (fn [x] {(sanatize-keyword (first x)) (into #{} (map sanatize-keyword (take 4 (rest x))))}) (doall (csv/read-csv reader :separator \;)))))
