9d9b4b84302a7b192829e4b7afbeac63d09bc901
gganley <gganley@student.bridgew.edu>
2018-12-19 12:07:03 -0500

    Include CSV dependency

--- src/skyrim.clj
+++ src/skyrim.clj
@@ -1,6 +1,7 @@
 (ns skyrim
-  (:require [clojure.core.async :as async]))
-
+  (:require [clojure.core.async :as async]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]))
 
 
 
